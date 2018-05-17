cd9e1505f0dcf796a26141cc69669aa580a19ea4
Daniel de Salabert Garcia <danielgdesalabert@gmail.com>
2018-05-17 17:37:33 +0200

    made index and his route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.master')
+@section('titlePage')
+index
+@stop
+@section('content')
+
+<h3>Benvinguts al portal de gestió de tasques</h3>
+<p>
+En aquest portal podrás gestionar tasques segons l'usuari de forma fàcil i ràpida.
+</p>
+
+@stop
+@section('footer')
+<h3>App development</h3>
+@stop
